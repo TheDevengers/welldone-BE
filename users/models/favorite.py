a3520911a8c375cd624bd5d2135fe0788bdd8da9
Pablo L Basanta <basanta79@gmail.com>
2019-09-08 00:10:52 +0200

    WIP models migrated

--- users/models/favorite.py
+++ users/models/favorite.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from articles.models import Article
 
 class Favorite(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    article = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True) 
 
     def __str__(self):
-        return self.article.title
+        return self.article_id.title
--- users/models/follower.py
+++ users/models/follower.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Follower(models.Model):
-    follower_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    followed_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    follower_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower_of')
+    followed_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followed_by')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.follower_id.username + '->' + self.followed_id.username
-    
