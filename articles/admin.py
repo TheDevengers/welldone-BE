24d809350de5446f9ef8205d536f8fd92da434a1
Pablo L Basanta <basanta79@gmail.com>
2019-09-11 01:09:33 +0200

    register models in admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from articles.models import Article, Category, Comment, Highlight
 
-# Register your models here.
+admin.site.site_header = "Welldone admin"
+admin.site.site_title = "Welldone admin portal"
+admin.site.index_title = "Welcome to Welldone admin Portal"
+
+
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Highlight)
