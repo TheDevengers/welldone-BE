b2e9c089788d874f44d8244aa2711cbcff20a119
Vegekku <vegekku@gmail.com>
2019-09-12 15:23:33 +0200

    WIP Added limit for pagination

--- articles/views.py
+++ articles/views.py
@@ -7,11 +7,14 @@ from django.views import View
 
 from articles.models import Article
 
+DEFAULT_SHOWN = 1
+
 
 class LatestArticlesView(View):
     def get(self, request):
         page = request.GET.get('page')
-        shown = request.GET.get('shown', 10)
+        shown = request.GET.get('shown', DEFAULT_SHOWN)
+        shown_param = '&shown={0}'.format(shown) if shown != DEFAULT_SHOWN else ''
 
         article_list = Article.objects.select_related('author').all()\
             .filter(publication_date__lte=datetime.now(), state__exact='PB')\
@@ -20,7 +23,7 @@ class LatestArticlesView(View):
 
         articles = paginator.get_page(page)
 
-        context = {'latest_articles': articles}
+        context = {'latest_articles': articles, 'shown_param': shown_param}
 
         html = render(request, 'articles/latest.html', context)
 
