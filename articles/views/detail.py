59134b08799787e4c469a40df35b79a2134e8cfd
Ferveloper <fermervil@gmail.com>
2019-09-19 20:56:51 +0200

    WIP Get comments in ArticleDetailView

--- articles/views/detail.py
+++ articles/views/detail.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views import View
 
-from articles.models import Article
+from articles.models import Article, Comment
 
 
 class ArticleDetailView(View):
@@ -14,7 +14,11 @@ class ArticleDetailView(View):
 
         article = get_object_or_404(Article.objects.select_related('author'), Q(slug=slug) & Q(publication_date__lte=datetime.now()) & Q(state__exact='PB'))
 
-        context = {'article': article, 'username': username}
+        comments = Comment.objects.select_related('article').all().filter(article=article.pk)
+
+        context = {'article': article,
+                   'username': username,
+                   'comments': comments}
 
         html = render(request, 'articles/detail.html', context)
 
