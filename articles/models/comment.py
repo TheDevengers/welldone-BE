821fc850334dca2d2eff978fafe9465f2f0d6422
Ferveloper <fermervil@gmail.com>
2019-09-20 12:27:20 +0200

    WIP Change comments creation_date to DateTimeField

--- articles/models/comment.py
+++ articles/models/comment.py
@@ -8,7 +8,7 @@ class Comment(models.Model):
     text = models.TextField(max_length=500, verbose_name='Comment body')
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Comment author')
     article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='Article', related_name='comments')
-    creation_date = models.DateField(verbose_name='Creation date', auto_now_add=True)
+    creation_date = models.DateTimeField(verbose_name='Creation date', auto_now_add=True)
 
     def __str__(self):
         return self.title
