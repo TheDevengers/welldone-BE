204548a644a37af66b627ba9e5b7711ffab30da7
Ferveloper <fermervil@gmail.com>
2019-09-21 15:49:31 +0200

    WIP Filter articles by search query string

--- articles/controllers/articles.py
+++ articles/controllers/articles.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 DEFAULT_SHOWN = 10
 
@@ -9,12 +10,14 @@ class ListArticles(object):
 
     @staticmethod
     def filter(request, article_objects):
+        search = request.GET.get('search')
         page = request.GET.get('page')
         shown = request.GET.get('shown', DEFAULT_SHOWN)
         shown_param = '&shown={0}'.format(shown) if shown != DEFAULT_SHOWN else ''
 
         article_list = article_objects.select_related('author').all()\
             .filter(publication_date__lte=datetime.now(), state__exact='PB')\
+            .filter(Q(title__icontains=search) | Q(introduction__icontains=search) | Q(body__icontains=search))\
             .order_by('-publication_date')
         paginator = Paginator(article_list, shown)
 
