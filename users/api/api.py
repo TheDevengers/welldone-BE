0216a8f86fab3e4a61a00f3af53617d3823a7b25
Hector Vazquez <hvazquezglez@gmail.com>
2019-09-22 12:10:33 +0100

    * WIP Registration user

--- users/api/api.py
+++ users/api/api.py
@@ -1,11 +1,34 @@
-from django.contrib.auth.models import User
-
+from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.views import APIView
 
-class UsersAPI(APIView):
-  def post(self, request):
-    return Response()
+from django.contrib.auth import get_user_model
+
+from users.serializers import UserSignUpSerializer
+
+User = get_user_model()
+
+
+class UsersAPI(generics.CreateAPIView):
+
+    serializer_class = UserSignUpSerializer
+
+    def post(self, request, *args, **kwargs):
+        first_name = request.data.get('first_name')
+        last_name = request.data.get('last_name')
+        email = request.data.get('email')
+        username = request.data.get('username')
+
+        if not first_name or not last_name or not email or not username:
+            return Response({'detail': 'All The Fields Are Required'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer = UserSignUpSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                # user.set_password
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class UserAPI(APIView):
   def get(self, request, pk):
