b8688a0c6d35f695084787cb99c075c946f3c56c
Ferveloper <fermervil@gmail.com>
2019-09-23 22:11:43 +0200

    Refactor articles.py and add add search parameter

--- articles/controllers/articles.py
+++ articles/controllers/articles.py
@@ -10,17 +10,18 @@ class ListArticles(object):
 
     @staticmethod
     def filter(request, article_objects):
-        search = request.GET.get('search') if request.GET.get('search') else ''
+        search = request.GET.get('search', '').strip()
         page = request.GET.get('page')
         shown = request.GET.get('shown', DEFAULT_SHOWN)
         shown_param = '&shown={0}'.format(shown) if shown != DEFAULT_SHOWN else ''
+        search_param = 'search={0}&'.format(search) if search != '' else ''
 
         article_list = article_objects.select_related('author').all()\
             .filter(publication_date__lte=datetime.now(), state__exact='PB')\
             .order_by('-publication_date')
 
         article_list = article_list.filter(Q(title__icontains=search) | Q(introduction__icontains=search) | Q(body__icontains=search))\
-            .all()[:SEARCH_RESULTS] if request.GET.get('search') else article_list
+            .all()[:SEARCH_RESULTS] if search else article_list
 
         paginator = Paginator(article_list, shown)
 
@@ -29,7 +30,9 @@ class ListArticles(object):
         context = {
             'article_list': articles,
             'shown_param': shown_param,
+            'search_param': search_param,
             'page_title': 'Latest articles',
+            'search': search,
         }
 
         return context
