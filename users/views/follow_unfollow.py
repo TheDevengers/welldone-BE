f03b55890c4b98d8b5611f35336031aad7e2abce
Ferveloper <fermervil@gmail.com>
2019-10-03 13:55:05 +0200

    Prevent not authenticated users to follow/unfollow

--- users/views/follow_unfollow.py
+++ users/views/follow_unfollow.py
@@ -6,11 +6,13 @@ from users.controllers import FollowersController
 
 class FollowView(View):
     def post(self, request, username=None):
-        FollowersController.follow(request=request, username=username)
+        if request.user.is_authenticated:
+            FollowersController.follow(request=request, username=username)
         return redirect(request.headers.get('referer'))
 
 
 class UnfollowView(View):
     def post(self, request, username=None):
-        FollowersController.unfollow(request=request, username=username)
+        if request.user.is_authenticated:
+            FollowersController.unfollow(request=request, username=username)
         return redirect(request.headers.get('referer'))
