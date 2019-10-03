6a8eadbdc61d6a5f59d78677d76be8e98480a4a3
helltor <36117004+hvctgr@users.noreply.github.com>
2019-10-03 22:11:42 +0100

    Feature/user update (#24)
    * listar y borrar usuario
    
    * actualizar usuario, restriccion username y correo
    
    * cambio a ListCreateView
    
    * actualizacion, borrado y detalle de usuario con datos complementarios
    
    * creacion del usuario con el modelo profile vacio
    
    * Endpoints añadidos:
    
    - La creacion de usuario ahora crea una instancia vacia a profile cuando se crea. Los usuarios creados anteriormente darán fallo al actualizar si no tienen una tabla profile asignada (mediante el admin de django)
    
    - Listar todos los usuarios.
    
    - Ver todos los datos de un usuario (solo el propio usuario).
    
    - Actualizar un usuario con sus campos adicionales (solo el propio usuario).
    
    - Borrar un usuario (solo el propio usuario).
    
    Para autenticarnos a traves de POSTMAN, usar en Authorization -> Type: Bearer Token -> Token: (coger el que se crea en la cookie acceskey de react).
    
    Un ejemplo de formato de actualizacion seriael siguiente:
    
    {
      "first_name": "nombre",
      "last_name": "apellido",
      "email": "hello@gmail.com",
      "username": "hello",
      "date_joined": "2019-09-30T23:56:44.486610+02:00",
      "profile": {
        "description": "esto es una descripcion",
        "birth_date": "2009-09-30",
        "birth_place": "Jaen"
      }
    }

--- users/api/api.py
+++ users/api/api.py
@@ -1,18 +1,16 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
 
 from django.contrib.auth import get_user_model
-
-from users.serializers import UserSignUpSerializer
+from users.permissions import  UserPermission
+from users.serializers import UserSignUpSerializer, UserSerializer, UserListSerializer
 
 User = get_user_model()
 
 
-class UsersAPI(CreateAPIView):
-
-    serializer_class = UserSignUpSerializer
+class UsersAPI(ListCreateAPIView):
+    queryset = User.objects.all()
 
     def post(self, request, *args, **kwargs):
         first_name = request.data.get('first_name')
@@ -28,13 +26,13 @@ class UsersAPI(CreateAPIView):
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def get_serializer_class(self):
+        return UserListSerializer if self.request.method == 'GET' else UserSignUpSerializer
 
-class UserAPI(APIView):
-  def get(self, request, pk):
-    return Response()
 
-  def put(self, request, pk):
-    return Response()
+class UserAPI(RetrieveUpdateDestroyAPIView):
 
-  def delete(self, request, pk):
-    return Response()
+    permission_classes = [UserPermission]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
