d877a53436aad7894e17dcef5e54581bf0e585dd
Pablo L Basanta <basanta79@gmail.com>
2019-10-09 01:08:12 +0200

    WIP

--- articles/views/usershandler.py
+++ articles/views/usershandler.py
@@ -6,8 +6,15 @@ from django.contrib.auth.models import User
 class UserListView(View):
 
     def get(self, request):
-        users = User.objects.all()
+        search = request.GET.get('search-name')
+        if search:
+            users = User.objects.filter(username__icontains=search).order_by('username')
+        else:
+            users = User.objects.all().order_by('username')
         context = dict(
             users=users
         )
         return HttpResponse(render(request, 'articles/users.html', context))
+
+    def post(self, request):
+        pass
