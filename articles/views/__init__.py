fae0ea74cc05375147d380064c2f8a9c84bbb2b6
Pablo López Basanta <basanta79@gmail.com>
2019-10-10 23:52:26 +0200

    Feature/categories view (#32)
    * Redesign the header
    
    * Add Header and categories view

--- articles/views/__init__.py
+++ articles/views/__init__.py
@@ -1,2 +1,3 @@
 from .detail import ArticleDetailView, CommentsView, FavoriteView, ResponseToView
 from .lists import LatestArticlesView, AuthorArticlesView, CategoryArticlesView
+from .categories import CategoriesListView
